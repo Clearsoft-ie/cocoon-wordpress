5d09557c97e5af662d045558bb29bbd673bedb49
shane byrne <info@clearsoft.ie>
2018-09-20 19:44:18 +0100

    Added in hotel logos to footer and assets

--- footer.php
+++ footer.php
@@ -75,7 +75,22 @@
 
                                     </div>
                                     <!-- /Social Icons -->
-
+                                    <!-- Hotel Logos -->
+                                    <div class="mt-20">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <a href="http://www.ambersprings.ie/" target="_blank">
+                                                    <img src="<?php echo get_template_directory_uri() ;?>/assets/images/logos/amber-springs.png" class="img-fluid" />
+                                                </a>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <a href="https://www.ashdownparkhotel.com" target="_blank">
+                                                    <img src="<?php echo get_template_directory_uri() ;?>/assets/images/logos/ashdown-park.png" class="img-fluid" />
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- /  Hotel Logos -->
                                 </div>
 
                             </div>
