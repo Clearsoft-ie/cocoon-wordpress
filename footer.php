667f60ac185d32ba4f4a98f972e1d55e72db0324
shane byrne <info@clearsoft.ie>
2018-09-24 20:09:38 +0100

    removed ashdown

--- footer.php
+++ footer.php
@@ -78,12 +78,7 @@
                                     <!-- Hotel Logos -->
                                     <div class="mt-20">
                                         <div class="row">
-                                            <div class="col-md-6">
-                                            <a href="https://www.ashdownparkhotel.com" target="_blank">
-                                                    <img src="<?php echo get_template_directory_uri() ;?>/assets/images/logos/ashdown-park.png" class="img-fluid" />
-                                                </a>
-                                            </div>
-                                            <div class="col-md-6">
+                                            <div class="col-md-12">
                                             <a href="http://www.ambersprings.ie/" target="_blank">
                                                     <img src="<?php echo get_template_directory_uri() ;?>/assets/images/logos/amber-springs.png" class="img-fluid" />
                                                 </a>
